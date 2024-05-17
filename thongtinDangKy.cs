9a70d80412ec82aa9d4058645df35300ecb697fd
t-nhi <135095690+t-nhi@users.noreply.github.com>
2024-05-17 20:35:45 +0700

    Add files via upload

---
+++ thongtinDangKy.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebsiteQLHDTN.Models;
+
+namespace WebsiteQLHDTN.ViewModels
+{
+    public class thongtinDangKy
+    {
+        public List<string> tenban1 {  get; set; } = new List<string>();
+        public int Idhoatdong1 { get; set; }
+        public string? Tenhoatdong1 { get; set; }
+        public int Idcanhan1 { get; set; }
+        public DateOnly? Thoigiandangky1 { get; set; }
+
+        public string? Bandangky1 { get; set; }
+        [Required]
+        public string? Uudiem1 { get; set; }
+        [Required]
+        public string? Nhuocdiem1 { get; set; }
+
+        public bool? Trangthaiduyetdon1 { get; set; } = false;
+    }
+}
