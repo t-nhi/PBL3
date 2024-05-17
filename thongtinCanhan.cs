51162e9a2dc6bd998cfa9ce7e9657eeab402940d
t-nhi <135095690+t-nhi@users.noreply.github.com>
2024-05-17 20:31:11 +0700

    Add files via upload

---
+++ thongtinCanhan.cs
@@ -0,0 +1,24 @@
+﻿using WebsiteQLHDTN.Models;
+
+namespace WebsiteQLHDTN.ViewModels
+{
+    public class thongtinCanhan
+    {
+        public int Idcanhan { get; set; }
+
+        public string? Ten { get; set; }
+
+        public string? Sdt { get; set; }
+
+        public string? Diachi { get; set; }
+
+        public string? Email { get; set; }
+
+        public bool? Gioitinh { get; set; }
+
+        public int? Namsinh { get; set; }
+        public bool? Trangthai { get; set; }
+
+        public List<QuanlyTghd> dsHoatdong {  get; set; }
+    }
+}
