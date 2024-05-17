2dbcb9f84d758ef17c208e8a5f18539a587a624c
t-nhi <135095690+t-nhi@users.noreply.github.com>
2024-05-17 20:14:15 +0700

    Add files via upload

---
+++ BaocaoViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace WebsiteQLHDTN.ViewModels
+{
+    public class BaocaoViewModel
+    {
+        public int? Idtkbaocao { get; set; }
+
+        public int? Idtkbibaocao { get; set; }
+
+        public DateOnly? Tgbaocao { get; set; }
+
+        public string? Ndbaocao { get; set; }
+        public int? VTtaikhoanbc { get; set; }
+        public int? VTtaikhoanbibc { get; set; }
+    }
+}
